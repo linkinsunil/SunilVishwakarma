8f47a4cfc7d89e42c0daa5a9ac6ab7cf47d9cc4b
Sunil Vishwakarma <sunilnet4@gmail.com>
2023-11-02 14:40:55 +0530

    feat: added ui for intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center px-4 py-32">
+      <Intro />
+    </main>
+  );
 }
